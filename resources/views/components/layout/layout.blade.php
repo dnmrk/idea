d73e4e815538cab494f93acfe4de115d869f77b8
Dean Mark Lamzon <dnmrk.lmzn@gmail.com>
2026-03-22 16:16:02 +0800

    Flash Messaging and Interactivity with AlpineJS

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Idea</title>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Idea</title>
 
-		@vite(['resources/css/app.css'])
-	</head>
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-	<body class="bg-background text-foreground">
-		<x-layout.nav/>
+<body class="bg-background text-foreground">
+	<x-layout.nav />
 
-		<main
-			class="max-w-7xl mx-auto px-6 pb-10 py-10">{{ $slot }}
-		</main>
-	</body>
+	<main class="max-w-7xl mx-auto px-6 pb-10 py-10">
+		{{ $slot }}
+	</main>
+
+
+	@session('success')
+		<div x-data="{ show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+			x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+			{{ $value }}
+		</div>
+	@endsession
+
+</body>
 
 </html>
-
